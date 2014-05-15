805d9b006b1c565e5598f0a95366fc6a990ae262
antmachine <anthonysallows@gmail.com>
2014-05-15 14:54:14 -0700

    CreatePages: migrated (0.0122s)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515213920) do
+ActiveRecord::Schema.define(version: 20140515215337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20140515213920) do
   end
 
   add_index "adventures", ["library_id"], name: "index_adventures_on_library_id", using: :btree
+
+  create_table "pages", force: true do |t|
+    t.string   "name"
+    t.string   "text"
+    t.integer  "adventure_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "pages", ["adventure_id"], name: "index_pages_on_adventure_id", using: :btree
 
 end
