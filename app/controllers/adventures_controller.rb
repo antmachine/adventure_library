1743a0ce5b9e5c35adb3e1d628a3fc3c1c0d61a2
antmachine <anthonysallows@gmail.com>
2014-05-15 15:21:48 -0700

    added methods to adventures_controller

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,33 @@
 class AdventuresController < ApplicationController
+
+	def index
+		@adventures = Adventure.all
+
+		respond_to do |f|
+			f.html
+			f.json { render json: {adventures: @adventures.as_json(
+															except: [:id, :library_id]
+															include: {:pages => {except: :id}})} }
+			end		
+	end
+
+	def new
+		@adventure = Adventure.new
+	end
+
+	def create
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def show
+		
+	end
 end
