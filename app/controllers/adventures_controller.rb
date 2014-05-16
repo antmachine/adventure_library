3dd703b7016ff15c1918d01588a0e494775f9e09
antmachine <anthonysallows@gmail.com>
2014-05-15 17:53:41 -0700

    linkify works

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,12 +3,12 @@ class AdventuresController < ApplicationController
 	def index
 		@adventures = Adventure.all
 
-		# respond_to do |f|
-		# 	f.html
-		# 	f.json { render json: {adventures: @adventures.as_json(
-		# 													except: [:id, :library_id]
-		# 													include: {:pages => {except: :id}})} }
-			# end		
+		respond_to do |f|
+			f.html
+			f.json { render json: {adventures: @adventures.as_json(
+															except: [:id, :library_id]
+															include: {:pages => {except: :id}})} }
+		end		
 	end
 
 	def new
@@ -17,7 +17,7 @@ class AdventuresController < ApplicationController
 
 	def create
 		adventure = Adventure.create adventure_params
-		redirect_to adventure_path		
+		redirect_to root_path
 	end
 
 	def edit
@@ -30,8 +30,12 @@ class AdventuresController < ApplicationController
 
 	def show
 		@adventure = Adventure.find(params[:GUID])
+		# TODO adventure_id?
 	end
 
+	def destroy
+		
+	end
 	private
 		def adventure_params
 			params.require(:adventure).permit(:title, :author, :GUID, :library_id, :created_at, :updated_at)
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,29 @@ class PagesController < ApplicationController
 	before_action :load_page
 	# TODO only needed routes get load_adventure and load_page
 
+
+
+def create
+	
+end
+
+def new
+	
+end
+
+def destory
+
+end
+
+def show
+	# @adventure = Adventure.find params[:adventure_id]
+	# @page = @adventure.pages.find params [:id]
+end
+
+private
 	def load_adventure
 		@adventure = Adventure.find params[:adventure_id]
 		redirect_to root_path if @adventure.blank?
-
 	end
 
 	def load_page
